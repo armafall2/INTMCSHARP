3a26fc649f3b63241180898cd8511bcc2cb51af8
armafall2 <mat.lur518@gmail.com>
2024-01-02 11:28:12 +0100

    test console

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Wkfnkjjsdnjkrdorld");
+            Console.WriteLine("Hello Wkfnkjjsdnjkrdorldlkf,l");
             Console.ReadKey();
         }
     }
