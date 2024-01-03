cc28008f886e13d4ccd18e63914254b133997e92
armafall2 <mat.lur518@gmail.com>
2024-01-03 11:59:25 +0100

    Ajout Serie 2

---
+++ Serie II/Ex1_Search.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie_II
+{
+    public static class Search
+    {
+        public static int LinearSearch(int[] tableau, int valeur)
+        {
+            if(tableau.Length == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                for(int i = 0; i < tableau.Length; i++)
+                {
+                    if(tableau[i] == valeur)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public static int BinarySearch(int[] tableau, int valeur)
+        {
+            //TODO
+            return -1;
+        }
+    }
+}
