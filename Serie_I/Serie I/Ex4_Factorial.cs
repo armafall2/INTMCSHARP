26e3487460799416e05d86916923218e86e2472b
armafall2 <mat.lur518@gmail.com>
2024-01-03 10:01:02 +0100

    Mise en forme Ex4_Factorial

--- Serie_I/Serie I/Ex4_Factorial.cs
+++ Serie_I/Serie I/Ex4_Factorial.cs
@@ -8,7 +8,6 @@ namespace Serie_I
 {
     public static class Factorial
     {
-
         /*
          la version récursive peut être plus lente mais plus simple à l'écriture donc plus facilement maintenable et produit
         un programme plus petit en taille pour un même résultat.
