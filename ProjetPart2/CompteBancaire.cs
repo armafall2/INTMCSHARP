4fd06146947d4acb6ad4e4bdec54bea847440222
armafall2 <mat.lur518@gmail.com>
2024-01-11 18:15:37 +0100

    Avancement part2

---
+++ ProjetPart2/CompteBancaire.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class GestionnairesCompte
+{
+    public int Identifiant { get; set; }
+    public string Type { get; set; }
+    public int nbrTransac { get; set; }
+
+}
+class ListageGestionnaireCompte
+{
+    List<GestionnairesCompte> gestionnairesComptes;
+
+    public ListageGestionnaireCompte()
+    {
+        gestionnairesComptes = new List<GestionnairesCompte>();
+    }
+
+    public bool CreateGestionnaire(int identifiant, string type, int nbrTransac)
+    {
+   
+
+        if (gestionnairesComptes.Any(c => c.Identifiant == identifiant))
+        {
+            Console.WriteLine("Le gestionnaire avec l'identifiant spécifié existe déjà.");
+            return false;
+        }
+
+        GestionnairesCompte gestionnaires = new GestionnairesCompte();
+
+        if((type == "Particulier" || type == "Entreprise") && nbrTransac > 0)
+        {
+            gestionnaires.Identifiant = identifiant;
+            gestionnaires.Type = type;
+            gestionnaires.nbrTransac = nbrTransac;
+
+            gestionnairesComptes.Add(gestionnaires);
+
+            return true;
+        }
+        return false;
+    }
+
+    public void AffGestionnaire()
+    {
+        foreach(var element in gestionnairesComptes)
+        {
+            Console.WriteLine($"Gestionnaire : Id : {element.Identifiant} Type : {element.Type} nbTransac : {element.nbrTransac}");
+        }
+            
+    }
+}
+
+class Transaction
+{
+    public int Identifiant { get; set; }
+    public DateTime DateEffet {get; set;}
+    public decimal Montant { get; set; }
+    public int? Expediteur { get; set; }
+    public int? Destinataire { get; set; }
+
+    public void AffUneTransac(Transaction transac)
+    {
+        Console.WriteLine($"ID : {transac.Identifiant} Date Effet : {transac.DateEffet} Montant : {transac.Montant} Exp : {transac.Expediteur} Dest : {transac.Destinataire}");
+    }
+}
+class ListageTransaction
+{
+    List<Transaction> transactions;
+
+    public ListageTransaction()
+    {
+        transactions = new List<Transaction>();
+    }
+
+    public bool AjouterTransaction(int identifiant, DateTime dateEffet, decimal montant, int expediteur, int destinataire)
+    {
+
+        if (transactions.Any(t => t.Identifiant == identifiant) && montant > 0)
+        {
+            Console.WriteLine($"Identifiant {identifiant} deja existant ou montant {montant} incorrect");
+            return false;
+        }
+        else
+        {
+           
+            Transaction nouvelleTransaction = new Transaction
+            {
+                Identifiant = identifiant,
+                DateEffet = dateEffet,
+                Montant = montant,
+                Expediteur = expediteur,
+                Destinataire = destinataire
+            };
+
+            transactions.Add(nouvelleTransaction);
+            return true;
+        }
+    }
+
+    public Transaction GetTransactionById(int identifiant)
+    {
+        return transactions.FirstOrDefault(t => t.Identifiant == identifiant);
+    }
+
+    public void AfficheTransac()
+    {
+        int cpt = 0;
+        Console.WriteLine("Liste des transactions :");
+        foreach (Transaction transaction in transactions)
+        {
+            Console.WriteLine($"Identifiant: {transaction.Identifiant}, Date : {transaction.DateEffet} Montant: {transaction.Montant}, Expediteur: {transaction.Expediteur}, Destinataire: {transaction.Destinataire}");
+            cpt++;
+        }
+
+        if (cpt == 0)
+        {
+            Console.WriteLine($"Liste transaction vide.");
+        }
+    }
+}
+
+class CompteBancaire
+{
+    public int Identifiant { get; set; }
+    public DateTime DateEffet { get; set; }
+    public decimal Solde { get; set; }
+    public int? Entrer { get; set; }
+    public int? sortie { get; set; }
+
+    public List<decimal> derniersRetraits = new List<decimal>();
+
+    public CompteBancaire()
+    {
+        Solde = 0;
+    }
+
+    public CompteBancaire(int identifiant, decimal soldeInitial)
+    {
+        Identifiant = identifiant;
+        Solde = soldeInitial;
+    }
+
+    public void test()
+    {
+        foreach (var element in derniersRetraits)
+        {
+            Console.WriteLine(element);
+        }
+    }
+}
+
+class ListageCompteBancaire
+{
+    List<CompteBancaire> listeCompteBancaires;
+
+    public ListageCompteBancaire()
+    {
+        listeCompteBancaires = new List<CompteBancaire>();
+    }
+
+    public bool CreateBankAccount(int dernierIdentifiant, DateTime date, decimal montantInitial, int? entre, int? sortie)
+    {
+
+        if (listeCompteBancaires.Any(c => c.Identifiant == dernierIdentifiant))
+        {
+            Console.WriteLine("Le compte avec l'identifiant spécifié existe déjà.");
+            return false;
+        }
+
+        if (montantInitial < 0)
+        {
+            Console.WriteLine("Solde nul");
+            return false;
+        }
+        if(date == null)
+        {
+            Console.WriteLine("Date nul");
+            return false;
+        }
+        CompteBancaire nouveauCompte = new CompteBancaire();
+
+        if (montantInitial.Equals(null) || montantInitial == 0)
+        {
+            nouveauCompte.Identifiant = dernierIdentifiant;
+            nouveauCompte.Solde = 0;
+            nouveauCompte.DateEffet = date;
+            nouveauCompte.Entrer = entre;
+            nouveauCompte.sortie = sortie;
+
+
+        }
+        else
+        {
+            nouveauCompte.Identifiant = dernierIdentifiant;
+            nouveauCompte.Solde = montantInitial;
+            nouveauCompte.DateEffet = date;
+            nouveauCompte.Entrer = entre;
+            nouveauCompte.sortie = sortie;
+        }
+
+        listeCompteBancaires.Add(nouveauCompte);
+        return true;
+    }
+    
+    public void AffCompte()
+    {
+        foreach(var element in listeCompteBancaires)
+        {
+            Console.WriteLine($"Id : {element.Identifiant}, Date : {element.DateEffet}, montant : {element.Solde}, Entrer : {element.Entrer} Sortie : {element.sortie}");
+        }
+    }
+
+}
---
+++ ProjetPart2/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetPart2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = Directory.GetCurrentDirectory();
+
+            for (int i = 1; i < 2; i++)
+            {
+                try
+                {
+                    #region Files
+                    // Input
+                    string mngrPath = path + $@"\Gestionnaires_{i}.txt";
+                    string oprtPath = path + $@"\Comptes_{i}.txt";
+                    string trxnPath = path + $@"\Transactions_{i}.txt";
+                    // Output
+                    string sttsOprtPath = path + $@"\StatutOpe_{i}.txt";
+                    string sttsTrxnPath = path + $@"\StatutTra_{i}.txt";
+                    string mtrlPath = path + $@"\Metrologie_{i}.txt";
+                    #endregion
+                    StreamReader gestionFile = new StreamReader(mngrPath);
+                    StreamReader comptesFile = new StreamReader(oprtPath);
+                    StreamReader transactionsFile = new StreamReader(trxnPath);
+                    StreamWriter sttOperFile = new StreamWriter(sttsOprtPath);
+                    StreamWriter sttTranFile = new StreamWriter(sttsTrxnPath);
+                    StreamWriter metroloFile = new StreamWriter(mtrlPath);
+
+
+                    ListageGestionnaireCompte listageGestionnaireCompte = new ListageGestionnaireCompte();
+                    ListageTransaction listageTransaction = new ListageTransaction();
+                    Transaction transaction = new Transaction();
+
+                    ListageCompteBancaire listageCompteBancaire = new ListageCompteBancaire();
+                    CompteBancaire compteBancaire = new CompteBancaire();
+
+                    #region Main
+                    if (File.Exists(mngrPath) && File.Exists(oprtPath) && File.Exists(trxnPath))
+                    {
+
+                        while (!gestionFile.EndOfStream)
+                        {
+                            string line = gestionFile.ReadLine();
+                            string[] stk = line.Split(';');
+
+                            if (int.TryParse(stk[0], out int id) && (stk[1] == "Particulier" || stk[1] == "Entreprise") && int.TryParse(stk[2], out int nbrTransac) && nbrTransac >= 0)
+                            {
+                                listageGestionnaireCompte.CreateGestionnaire(id, stk[1], nbrTransac);
+                            }
+
+                        }
+                        listageGestionnaireCompte.AffGestionnaire();
+
+                        Console.WriteLine(" ");
+
+                        while (!transactionsFile.EndOfStream)
+                        {
+
+                            string line = transactionsFile.ReadLine();
+                            string[] stk = line.Split(';');
+                            string res = "";
+                            DateTime tmp = DateTime.Now;
+                            decimal montant = 0;
+                            int exp = 0;
+                            int dest = 0;
+                            i = 0;
+
+                            if (int.TryParse(stk[0], out int id) && DateTime.TryParse(stk[1], out tmp) && decimal.TryParse(stk[2], out montant) && int.TryParse(stk[3], out exp) && int.TryParse(stk[4], out dest) && stk.Length == 5)
+                            {
+                                listageTransaction.AjouterTransaction(id, tmp, montant, exp, dest);
+                            }
+                            else
+                            {
+                                for (int j = 0; j < stk.Length; j++)
+                                {
+                                    res += stk[j] + ";";
+                                }
+                                Console.WriteLine("Erreur : " + res);
+                            }
+
+                        }
+                        listageTransaction.AfficheTransac();
+
+                        Console.WriteLine(" ");
+
+                        while (!comptesFile.EndOfStream)
+                        {
+                            string line = comptesFile.ReadLine();
+                            string[] stk = line.Split(';');
+                            string res = "";
+                            DateTime tmp = DateTime.Now;
+                            decimal montant = 0;
+                            int expValue = 0;
+                            int destValue = 0;
+
+                            if (stk.Length == 5 &&
+                                int.TryParse(stk[0], out int id) &&
+                                DateTime.TryParse(stk[1], out tmp) &&
+                                decimal.TryParse(stk[2], out montant))
+                            {
+                                int? exp = null;
+                                if (!int.TryParse(stk[3], out expValue))
+                                {
+                                    Console.WriteLine($"Erreur : La valeur pour Entrer n'est pas un nombre valide.");
+
+                                }
+                                else
+                                {
+                                    if (string.IsNullOrEmpty(stk[3]))
+                                    {
+                                        exp = null;
+                                    }
+                                    else
+                                    {
+                                        exp = expValue;
+                                    }
+                                    
+                                }
+
+                                int? dest = null;
+                                if (!int.TryParse(stk[4], out destValue))
+                                {
+                                    Console.WriteLine($"Erreur : La valeur pour Sortie n'est pas un nombre valide.");
+                                }
+                                else
+                                {
+                                    if (string.IsNullOrEmpty(stk[4]))
+                                    {
+                                        dest = null;
+                                    }
+                                    else
+                                    {
+                                        dest = destValue;
+                                    }
+                                    
+                                }
+
+                                listageCompteBancaire.CreateBankAccount(id, tmp, montant, exp, dest);
+
+                            }
+                            else
+                            {
+                                for (int k = 0; k < stk.Length; k++)
+                                {
+                                    res += stk[k] + ";";
+                                }
+                                Console.WriteLine("Erreur : " + res);
+                            }
+
+
+
+                        }
+
+
+                        listageCompteBancaire.AffCompte();
+
+
+                        Console.WriteLine(" ");
+                    }
+                    #endregion
+
+                    
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Une exception s'est produite : {ex.Message}");
+                }
+
+            }
+            Console.ReadKey();
+
+        }
+        public int? TryParseNullable(string val)
+        {
+            int outValue;
+            return int.TryParse(val, out outValue) ? (int?)outValue : null;
+        }
+
+    }
+}
+
